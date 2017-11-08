2b35d07be08a860f8725e3a67393e08e3e3e1086
alexbodn@gmail.com <alexbodn@gmail.com>
2017-11-08 16:11:47 +0200

    Bump version: 0.1.5 → 0.2.0

--- dbix/__init__.py
+++ dbix/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Alex Bodnaru"""
 __email__ = 'alexbodn@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 
 
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='dbix',
-    version='0.1.5',
+    version='0.2.0',
     description="load a perl DBIx::Class schema with python",
     long_description=readme + '\n\n' + history,
     author="Alex Bodnaru",
